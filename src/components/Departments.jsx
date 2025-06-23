80cefbb1aeed880d4c70235c82938c36b9a07b98
christopherchege <chrisshege35@gmail.com>
2025-06-24 00:50:22 +0300

    feat: center department cards grid with max width and horizontal auto margin

--- src/components/Departments.jsx
+++ src/components/Departments.jsx
@@ -22,7 +22,7 @@ const Departments = () => {
         Departments Overview
       </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
         {departments.map((dept) => (
           <div
             key={dept.id}
